fffd1ff2a2e77b60f88a9a98c658d782ec7af0d6
puchhas <127958930+puchhas@users.noreply.github.com>
2023-03-15 09:49:57 -0400

    Update streamlit_app.py
    added pictures

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner') 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega3 & Bulebeery Oat Meal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled range Egg')
+streamlit.text('🥣 Omega3 & Bulebeery Oat Meal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text(' 🐔Hard-Boiled range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
