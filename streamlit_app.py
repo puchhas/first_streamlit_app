581d90c6396d8944dc511bd7ef9a859a88ec3a0c
puchhas <127958930+puchhas@users.noreply.github.com>
2023-03-15 09:44:05 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner') 
-Breakfast Menu
+streamlit.title('Breakfast Menu')
 Omega3 & Bulebeery Oat Meal
 Kale, Spinach & Rocket Smoothie
 Hard-Boiled Egg
